3b55f2f652331ce384e3df48019b9e3ecb805269
mikhawa <michaeljpitz@gmail.com>
2019-11-22 16:32:12 +0100

    dump($util);

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Articles;
+use App\Entity\Utilisateurs;
 
 class BaseController extends AbstractController
 {
@@ -12,6 +14,11 @@ class BaseController extends AbstractController
      */
     public function index()
     {
+        $util = $this->getDoctrine()
+            ->getRepository(Utilisateurs::class)
+            ->find(1);
+
+        dump($util);
         return $this->render('base/index.html.twig', [
             'controller_name' => 'BaseController',
         ]);
